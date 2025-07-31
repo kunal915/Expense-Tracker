17030193fb8c1e9842e87b853da926cbe510170c
Kunal <kunalra79@gmail.com>
2025-07-31 11:37:42 +0530

    Update firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -3,14 +3,7 @@ import { initializeAuth, inMemoryPersistence, setPersistence } from 'firebase/au
 import { getFirestore } from 'firebase/firestore';
 
 // Your Firebase configuration object
-const firebaseConfig = {
-  apiKey: "AIzaSyBcXHwa5BJshwpQud8u94vbPx17eDM80Us",
-  authDomain: "expense-tracker-f6a26.firebaseapp.com",
-  projectId: "expense-tracker-f6a26",
-  storageBucket: "expense-tracker-f6a26.firebasestorage.app",
-  messagingSenderId: "570575185258",
-  appId: "1:570575185258:web:e134dad825ef52e9883113"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
